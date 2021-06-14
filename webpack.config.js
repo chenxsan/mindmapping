ffe0df0cec40661800631740417ff10857507400
chenxsan <chenxsan@gmail.com>
2021-06-14 21:24:54 +0800

    add babel etc.

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/i,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '...'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'MindMapping',
+    }),
+  ],
+}
