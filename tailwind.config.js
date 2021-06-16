bf9c2a7a0c8b972b85034e24727a5db2ba1c582b
chenxsan <chenxsan@gmail.com>
2021-06-16 22:28:45 +0800

    use tailwindcss@canary

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.{ts,tsx}'],
+  purge: ['./src/**/*.{ts,tsx}', './src/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors,
     extend: {},
   },
   variants: {
